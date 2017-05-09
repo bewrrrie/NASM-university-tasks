b4ac42f07da2b4066d006c53a4b657f01c606f2b
bewrrrie <bewrrrie@localhost.localdomain>
2017-05-09 14:39:21 +0600

    final version. Finally!

--- second/SecondTask.asm
+++ second/SecondTask.asm
@@ -1,6 +1,6 @@
 ;Count matrix columns that
 ;contain only elements with absolute value
-;less than given positive number (named "value").
+;less than given positive number (named "x").
 ;Matrix is represented as array of lines.
 
 section .bss
@@ -11,9 +11,9 @@ section .bss
 
 section .data
     format_input: dd "%d %d %d/n", 10, 0
-    format_input_element: dd "%d", 10, 0
-    format_out_result: dd "%d", 10, 0
-    format_out_element: dd "%d ", 10, 0
+    format_input_element: dd "%d ", 10, 0
+    format_output_result: dd "result:%d", 10, 0
+    format_output_element: dd "%d ", 10, 0
 
 section .text
 extern scanf
@@ -32,125 +32,126 @@ main:
     call    scanf
     add     esp, 16
 
+    mov     dword eax, [m_width];   eax <- width.
+    cmp     eax, 0;                 if matrix has zero width
+    je      END;                    then finish.
+
+    mov     dword eax, [m_height];  eax <- height.
+    cmp     eax, 0;                 if matrix has zero height
+    je      END;                    then finish.
+
     call    allocateMatrixMemory
     call    readMatrix
-    call    writeMatrix
-    call    findCol
+    call    countColsWithAbsValueLessThanX
+    
+    push    eax
+    push    format_output_result
+    call    printf
+    add     esp, 8
+    END:
     ret;    RETURN
+
 
 
 allocateMatrixMemory:
 ;Allocate matrix memory using matrix size(m_width, m_height).
 ;Result(beginning addres) -> matrix (variable, begin addres).
-    mov     esi, [m_height]
-    shl     esi, 2
-    push    esi
-    call    dword malloc;   result: lines array addres -> eax
-    mov     [matrix], eax
-    mov     ebx, eax;       base addres to ebx
-    pop     esi;            clearing stack
-    shr     esi, 2
-    ;matrix height -> esi
-    xor     ecx, ecx
-    CYCLE:
-        push    ebx;    saving array addres to stack
-        push    ecx;    saving counter
-        push    esi;    saving matrix height
 
-        mov     edi, [m_width]; preparing size
-        shl     edi, 2;         ...
-        push    edi;            pushing to stack
-        call    dword malloc;   calling c function
-        add     esp, 4;         clearing stack
+    mov     dword eax, [m_height];  eax <- height.
+    shl     eax, 2;                 eax to bytes
+    push    eax
+    call    dword malloc;           allocating memory for lines array
+    mov     dword [matrix], eax;    'matrix' <- lines array beginnig addres
+    add     esp, 4;                 clear stack
 
-        pop     esi;    getting back matrix height
-        pop     ecx;    getting back counter
-        pop     ebx;    returning matrix base addres
-
-        mov     [ebx + ecx * 4], eax
-
+    mov     dword edi, [m_width]
+    shl     edi, 2;                 edi <- line size in bytes.
+    mov     dword edx, [m_height];  edx <- lines quantity.
+    mov     dword ebx, [matrix];    ebx <- lines array base addres.
+    xor     ecx, ecx;               ecx <- zero, index container.
+    ALLOC_CYCLE:
+        push    ecx;                    SAVING EVERYTHING
+        push    edx;                    TO STACK
+        push    ebx
+        push    edi
+        call    dword malloc
+        pop     edi
+        pop     ebx;                    RETURNING EVERYTHING
+        pop     edx;                    FROM STACK
+        pop     ecx
+        mov     dword [ebx + ecx * 4], eax;     writing line addres to lines array
         inc     ecx
-        cmp     ecx, esi
-        jb      CYCLE
-    EXIT:
-    ret;    RETURN
+        cmp     ecx, edx
+    jb      ALLOC_CYCLE
+    ret
+
 
 
 readMatrix:
-;Read matrix from standard user input.
-;Matrix address in eax.
-;(eax <- matrix base addres)
-    xor     ecx, ecx;           line index
-    xor     edx, edx;           column index
+;Read matrix elements from standard user input.
 
-    mov     ebx, [matrix];      lines array base addres
-    mov     edi, [m_width];     width storage
-    mov     esi, [m_height];    height storage
+    mov     ebx, [matrix];  ebx <- lines array base addres
+    xor     esi, esi;       esi <- line index
+    READ_CYCLE_LINES:
+        push    ebx
+        push    esi
 
-    CYCLE_LINES_R:
-        push    ecx;            saving line index
-        push    esi;            saving height
-        CYCLE_COLUMNS_R:
-            push    edx;        saving column index
-            push    ebx;        saving lines array base addres
+        mov     edx, [ebx + esi * 4];   edx <- current line base addres
+        xor     edi, edi;               edi <- column index
+        READ_CYCLE_COLUMNS:
+            lea     ecx, [edx + edi * 4]
 
-            mov     eax, [ebx]; getting current line base addres
-            shl     edx, 2;     converting index to bytes
-            add     eax, edx;   adding index to base addres
-            push    eax
+            push    edi;                saving col.index to stack
+            push    edx
+            push    ecx
             push    format_input_element
             call    scanf
             add     esp, 8
+            pop     edx
+            pop     edi;                returning col.index from stack
 
-            pop     ebx;        getting back lines array base addres
-            pop     edx;        getting back column index
-            inc     edx
-            cmp     edx, edi
-            jb      CYCLE_COLUMNS_R
-        pop     esi;            getting back height
-        pop     ecx;            getting back line index
-
-        add     ebx, 4;         moving pointer through lines array
-        inc     ecx
-        cmp     ecx, esi
-        jb      CYCLE_LINES_R
-    ret;        RETURN
-
-
-writeMatrix:
-    xor     esi, esi;   line index
-    xor     edi, edi;   column index
-    mov     ebx, [matrix]
-    CYCLE_LINES_W:
-        mov     eax, [ebx]
-        push    esi
-        push    ebx
-        CYCLE_COLUMNS_W:
-            push    edi
-            shl     edi, 2
-            add     eax, edi
-
-            push    eax
-            push    format_out_element
-            call    printf
-            add     esp, 8
-
-            pop     edi
             inc     edi
             cmp     edi, [m_width]
-            jb      CYCLE_COLUMNS_W
-        pop     ebx
+            jb      READ_CYCLE_COLUMNS
         pop     esi
+        pop     ebx
 
-        add     ebx, 4
         inc     esi
         cmp     esi, [m_height]
-        jb      CYCLE_LINES_W
-    ret;        RETURN
+        jb      READ_CYCLE_LINES
+    ret
 
 
-findCol:
-;Finding index column that contains only
-;elements with absolute value less than x.
-    
-    ret;    RETURN
+
+countColsWithAbsValueLessThanX:
+;Count matrix columns that contain only elements
+;with absolute value less than x.
+
+    xor     eax, eax;       eax <- columns quantity
+    mov     ebx, [matrix];  ebx <- lines array base addres
+    xor     edi, edi;       esi <- column index
+    COUNT_CYCLE_COLUMNS:
+        xor     esi, esi;   esi <- line index
+        COUNT_CYCLE_LINES:
+            mov     edx, [ebx + esi * 4]
+            mov     edx, [edx + edi * 4];   edx <- current element
+
+;           abs(x) = (x xor y) - y
+;           where y = x >> 31
+            mov     ecx, edx
+            sar     ecx, 0x1f
+            xor     edx, ecx
+            sub     edx, ecx;   edx <- cur.element absolute value
+
+            cmp     edx, [x]
+            jge     ITER_END
+
+            inc     esi
+            cmp     esi, [m_height]
+            jb      COUNT_CYCLE_LINES
+        inc     eax;            increasing columns quantity
+        ITER_END:
+        inc     edi
+        cmp     edi, [m_width]
+        jb      COUNT_CYCLE_COLUMNS
+    ret
--- third/ThirdTask.asm
+++ third/ThirdTask.asm
@@ -1,14 +1,110 @@
-;Метод прямоугольников.
-;В качестве примера, реализовать вычисление
-;интеграла cos(x), x, exp(x).
+;Rectangle method (definite integral).
+;As an example integrate functions cos(x), x, exp(x).
 
 section .bss
+    a: resq 1;      | definite integral with
+    b: resq 1;      | integration borders - [a,b].
+    tmp: resq 1;    - temporary real number storage.
+
+    n: resd 1;  segments quantity.
+
+section .data
+    format_input: dd "%lf %lf %d ", 10, 0
+    format_output: dd "%lf ", 10, 0
 
 section .text
+extern scanf
+extern scanf_s
 extern printf
-extern malloc
 global main
+
+
+;USER INPUT:
+;   left integration border;
+;   right integration border;
+;   segments quantity (segmentation of integration area).
+
 main:
+    mov     ebp, esp;   for correct debugging
+
+    push    n
+    push    b
+    push    a
+    push    format_input
+    call    scanf
+    add     esp, 16
+
+    call    integrateFunction
+    call    printCoProcessorsStackTop
+
+    ret
+
+
+Function:
+;Function that is gonna be integrated.
+;(When called it is supposed that argument is in the top of coprocessor stack)
+    fsin
+    ret
+
+
+integrateFunction:
     finit
-    
+;                           STACK STATES CHANGING:
+;                          st0:     |st1:       |st2:           |st3:
+;                                   |           |               |
+    fldz;                  summ = 0 |           |               |
+    fld     qword [b];      b       | summ = 0  |               |
+    fld     qword [a];      a       | b         | summ = 0      |
+
+    fcom    st1
+    fstsw   ax
+    sahf
+    jc      OK
+    jz      EQUALS; Return zero if a=b.
+
+;   This block is executed when a>b:
+    fxch    st1
+
+    OK:
+;                          st0:     |st1:       |st2:           |st3:
+    fsubp   st1, st0;       (b-a)   | summ      |               |
+    fild    qword [n];      n       | (b-a)     | summ          |
+    fdivp   st1, st0;       (b-a)/n | summ      |               |
+    fld     qword [a];      a       | (b-a)/n   | summ          |
+;                                   |           |               |
+    fld1;                   i = 0   | a         |step:=(b-a)/n  | summ
+    mov     ecx, [n]
+    SUMM_CYCLE:;            st0:    |st1:   |st2:   |st3:       |st4:
+;                            i      | a     | step  | summ      |
+        fld     st0;         i      | i     | a     | step      | summ
+        fmul    st0, st3;    i*step | i     | a     | step      | summ
+        fadd    st0, st2;  i*step+a | i     | a     | step      | summ
+        fld1
+        fld1
+        faddp   st1, st0;        2  |i*st.+a| i     | a         | step  | summ
+        fdivp   st1, st0;    (..)/2 | i     | a     | step      | summ
+
+        call    Function;    F(..)  | i     | a     | step      | summ
+        fmul    st0, st3; F(..)*st. | i     | a     | step      | summ
+;                                   |       |       |
+        faddp   st4, st0;    i      | a     | step  | summ + F(..)
+;                                   |       |       |
+        fld1;                1      | i     | a     | step      | summ + F(..)
+        faddp   st1, st0;    i + 1  | a     | step  | summ + cos|
+    loop    SUMM_CYCLE
+    fstp    st0
+    EQUALS:
+    fstp    st0
+    fstp    st0
+    ret
+
+
+printCoProcessorsStackTop:
+    sub     esp, 8
+    fst     qword [esp]
+    lea     eax, [format_output]
+    push    eax
+    call    printf
+    add     esp, 12
+
     ret
